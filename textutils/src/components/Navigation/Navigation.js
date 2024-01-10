154e3ffa3ac3b79d64b1f175367ed134fec0edbe
raj <rajm267747@gmail.com>
2024-01-11 00:17:00 +0530

    done

---
+++ textutils/src/components/Navigation/Navigation.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import './Navigation.css'
+
+export default function Navigation() {
+  return (
+    <div>
+        <div className="main">
+            <div className="container containerf">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                    Flights
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                    Hotels
+                </div>
+            </div>
+
+            <div className="container">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                    Homestays<br/>&Villas
+                </div>
+            </div>
+            
+
+            <div className="container">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                    Holiday<br/>
+                    Packages
+                </div>
+            </div>
+
+
+            <div className="container">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                   Trains
+                </div>
+            </div>
+
+
+            <div className="container">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                   Buses
+                </div>
+            </div>
+
+
+            <div className="container">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                   Cabs
+                </div>
+            </div>
+
+
+            <div className="container containerl">
+                <div className="img">
+                   <img src="" alt="" />
+                </div>
+                <div className="content">
+                    Forex Card <br/>
+                    &Currency
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
---
+++ travel/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/Header.js';
+
+
+function App() {
+  return (
+    <div>
+      
+       <Header/>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+  );
+}
+
+export default App;
